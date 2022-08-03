ea02a0ee05c70b2ea4bc2815c0359ca8f2d8b431
jwaine44 <jwaine44@gmail.com>
2022-08-03 18:02:24 -0400

    Initialize Repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveywModel.Models;
+
+namespace DojoSurveywModel.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("")]
+    public IActionResult SurveySubmission(Survey result)
+    {
+        if(ModelState.IsValid)
+        {
+            return RedirectToAction("result", result);
+        }
+        else
+        {
+            return View("Index");
+        }
+    }
+
+    [HttpGet("result")]
+    public IActionResult Result(Survey result)
+    {
+        return View(result);
+    }
+
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,20 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+namespace DojoSurveywModel.Models;
+
+public class Survey
+{   
+    [Required]
+    [MinLength(2)]
+    [Display(Name = "Your Name:")]
+    public string Name {get;set;}
+    [Required]
+    [Display(Name = "Dojo Location:")]
+    public string Location {get;set;}
+    [Required]
+    [Display(Name = "Favorite Language:")]
+    public string FavLanguage {get;set;}
+    [MinLength(21)]
+    [Display(Name = "Comment (optional):")]
+    public string? Comment {get;set;}
+}
